d8885f085f3aba9b08a63589e14dc8a201da6265
Netaly Klemann <netalyk@mail.tau.ac.il>
2022-12-29 17:49:41 +0200

    finish exe+bonus

---
+++ README.md
@@ -0,0 +1,9 @@
+# chat-server
+
+This app is a backend implementation for a chat application, using mock data.
+
+## Instructions
+
+1. Clone the repository.
+2. Run `npm install`
+3. Run `npm run start-server`
--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,18 +1,38 @@
-import {Router,Request,Response} from "express";
+import {Router, Request, Response} from "express";
 import {Message} from "../types/message";
 import {mockUserDetails} from "../assets/mockUserDetails";
 import {mockMessages} from "../assets/mockMessages";
+import bodyParser from 'body-parser';
 
 export const messages = Router();
-function addAuthorNameToMessages() {
+
+function addAuthorNameToMessages(): Message[] {
     return mockMessages.map((message: Message) => {
         const author = mockUserDetails.find(user => user.id === message.authorId);
         const authorName = author && author.name;
         return {...message, authorName};
     });
 }
+
 messages.get('/', (req: Request, res: Response) => {
     res.send(addAuthorNameToMessages());
 });
 
+messages.post('/', bodyParser.json(), (req: Request, res: Response) => {
+    const message: Message = req.body;
+    mockMessages.push({
+        ...message,
+        likes: [],
+        authorName: mockUserDetails.find(user => user.id === message.authorId).name,
+        status: 'pending'
+    })
+    res.status(200).send()
+});
+
+messages.post('/:id', bodyParser.json(), (req: Request, res: Response) => {
+    const id = req.params?.id;
+    const {userId, like} = req.body;
+    const message = mockMessages.find(message => message.id === +id);
+    like ? message.likes!.splice(message.likes!.indexOf(userId), 1) : message.likes!.push(userId);
+});
 export default messages;
